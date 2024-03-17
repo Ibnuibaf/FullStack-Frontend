72e204adf422a53d0695ceaf2bf995361abc4749
Ibrahim Bin Aboobacker CH <91882961+Ibnuibaf@users.noreply.github.com>
2024-03-17 16:21:52 +0530

    Completion: Token to localStr

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,8 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
+import Cookies from "js-cookie";
 
+const token = Cookies.get("token");
 const api = axios.create({
-  baseURL: 'https://fullstack-backend-8lkr.onrender.com/api/',
-  withCredentials:true
+  baseURL: "https://fullstack-backend-8lkr.onrender.com/api/",
+  headers: {
+    Authorization: token || "",
+  },
 });
 
 export default api;
