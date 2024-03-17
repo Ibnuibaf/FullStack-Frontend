ba6eac9031d397cfc97a8b233407cd8ed4291aec
Ibrahim Bin Aboobacker CH <91882961+Ibnuibaf@users.noreply.github.com>
2024-03-17 15:19:50 +0530

    Completion: Dynamiced Analytics data

--- src/components/cards/ReviewAnalyseCard.jsx
+++ src/components/cards/ReviewAnalyseCard.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../redux/slices/userSlice";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../../axios/api";
 
 function ReviewAnalyseCard() {
   const user = useSelector(selectUser);
+  const [analitics, setAnalitics] = useState([]);
+  const getAnalytics = async () => {
+    try {
+      const res = await api.get("/review/analitics");
+      setAnalitics(res.data.analitics);
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  useEffect(() => {
+    getAnalytics();
+  }, []);
   return (
     <div className="px-10 py-5 grid grid-cols-2 gap-5">
       <div className="bg-slate-800 rounded-3xl p-4">
@@ -12,24 +27,24 @@ function ReviewAnalyseCard() {
           Request Analytics
         </p>
         <div className="flex flex-col items-center justify-between px-5 mt-3 gap-5">
-          <div className="bg-slate-300 w-full rounded-md font-semibold text-lg p-2 text-orange-700">
-            <p>Pending:</p>
-            <p className="italic text-xl font-bold text-slate-700 transition-all duration-500 animate-pulse">
-              129
-            </p>
-          </div>
-          <div className="bg-slate-300 w-full rounded-md font-semibold text-lg p-2 text-green-700">
-            <p>Fullfilled:</p>
-            <p className="italic text-xl font-bold text-slate-700 transition-all duration-500 animate-pulse">
-              129
-            </p>
-          </div>
-          <div className="bg-slate-300 w-full rounded-md font-semibold text-lg p-2 text-pink-700">
-            <p>Rejected:</p>
-            <p className="italic text-xl font-bold text-slate-700 transition-all duration-500 animate-pulse">
-              129
-            </p>
-          </div>
+          {analitics.map((stat) => (
+            <div
+              className={`bg-slate-300 w-full rounded-md font-semibold text-lg p-2 text-${
+                stat.status == "pending"
+                  ? "orange"
+                  : stat.status == "approved"
+                  ? "green"
+                  : stat.status == "rejected"
+                  ? "pink"
+                  : ""
+              }-700`}
+            >
+              <p>{stat.status.toUpperCase()}:</p>
+              <p className="italic text-xl font-bold text-slate-700 transition-all duration-500 animate-pulse">
+                {stat.count}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="bg-slate-800 rounded-3xl p-4 text-white flex flex-col justify-between min-w-60">
@@ -41,7 +56,7 @@ function ReviewAnalyseCard() {
           </p>
           <label htmlFor="">Role</label>
           <p className="text-lg text-center font-semibold bg-gray-500 text-pink-700/80 mb-4">
-            {user.data.role&&user.data.role.toUpperCase()}
+            {user.data.role && user.data.role.toUpperCase()}
           </p>
           <p className="text-gray-400 text-center">
             Welcome to your profile page. Here you can view and manage your
