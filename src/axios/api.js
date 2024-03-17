dd4d6a4432d2a1fdccccd788b62767c55b8861f9
Ibrahim Bin Aboobacker CH <91882961+Ibnuibaf@users.noreply.github.com>
2024-03-17 15:43:51 +0530

    completion: Domain change

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://fullstack-backend-8lkr.onrender.com/api/',
   withCredentials:true
 });
 
